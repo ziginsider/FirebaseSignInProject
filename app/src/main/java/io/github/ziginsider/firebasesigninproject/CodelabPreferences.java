73111cd2d04a27edb8e685ffc09a7f4f955c972a
ziginsider <ziginsider@gmail.com>
2017-12-06 01:08:34 +0300

    Initial commit

---
+++ app/src/main/java/io/github/ziginsider/firebasesigninproject/CodelabPreferences.java
@@ -0,0 +1,9 @@
+package io.github.ziginsider.firebasesigninproject;
+
+/**
+ * Created by zigin on 06.12.2017.
+ */
+
+public class CodelabPreferences {
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+}
